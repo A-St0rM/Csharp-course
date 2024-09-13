376881c1f521c833e83cad96760dc80913529dab
AlissaStorm <Alissaalidib@hotmail.com>
2024-09-13 21:13:02 +0200

    added console app.

---
+++ csharpCourse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace csharpCourse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
