8b1c5ca8cc6a383ab30e65f00edfd8f0bf3b673c
AlissaStorm <Alissaalidib@hotmail.com>
2024-09-15 11:01:29 +0200

    worked on the calculator

--- csharpCourse/Program.cs
+++ csharpCourse/Program.cs
@@ -13,93 +13,90 @@ namespace csharpCourse
             Console.WriteLine("2. Subtraktion");
             Console.WriteLine("3. Multiplikation");
             Console.WriteLine("4. Division");
-            string valg = Console.ReadLine();
+            double input = Convert.ToDouble(Console.ReadLine());
 
-            userInput();
+            Console.WriteLine("Indtast tal 1");
+            double t1 = Convert.ToDouble(Console.ReadLine()); 
 
-            if (valg == "1")
-            {
-                addition();
+            Console.WriteLine("Indtast tal 2");
+            double t2 = Convert.ToDouble(Console.ReadLine());
+ 
+            //userInput();
+
+            if (input == 1)
+            {              
+                double result = addition(t1, t2);
+                Console.WriteLine("Resultatet er: " + result);
+
             }
-            else if (valg == "2")
+            else if (input == 2)
             {
-                subtraktion();
+                //subtraktion();
             }
-            else if (valg == "3")
+            else if (input == 3)
             {
-                multiplikation();
+               // multiplikation();
             }
-            else if (valg == "4")
+            else if (input == 4)
             {
-                division();
+                //division();
             }
 
             
-            static void userInput()
-            {
-                Console.WriteLine("Indtast tal 1");
-                string tal1 = Console.ReadLine();
 
-                Console.WriteLine("Indtast tal 2");
-                string tal2 = Console.ReadLine();
-
-                double t1 = System.Convert.ToDouble(tal1);
-                double t2 = System.Convert.ToDouble(tal2);
-            }
-
-            static double addition() //Skal ændre retur type og tilføje parametre
+            double addition(double t1, double t2) //Skal ændre retur type og tilføje parametre
             {             
-                return sum = t1 + t2;
-                Console.WriteLine("Resultatet er: " + sum);
+                return t1 + t2;
+                
             }
 
-            static void subtraktion()
-            {
-                Console.WriteLine("Indtast tal 1");
-                string tal1 = Console.ReadLine();
+            //static void subtraktion()
+            //{
+            //    Console.WriteLine("Indtast tal 1");
+            //    string tal1 = Console.ReadLine();
 
-                Console.WriteLine("Indtast tal 2");
-                string tal2 = Console.ReadLine();
+            //    Console.WriteLine("Indtast tal 2");
+            //    string tal2 = Console.ReadLine();
 
-                double t1 = System.Convert.ToDouble(tal1);
-                double t2 = System.Convert.ToDouble(tal2);
+            //    double t1 = System.Convert.ToDouble(tal1);
+            //    double t2 = System.Convert.ToDouble(tal2);
 
-                double sum = t1 - t2;
+            //    double sum = t1 - t2;
 
-                Console.WriteLine("Resultatet er: " + sum);
-            }
+            //    Console.WriteLine("Resultatet er: " + sum);
+            //}
 
-            static void multiplikation()
-            {
-                Console.WriteLine("Indtast tal 1");
-                string tal1 = Console.ReadLine();
+            //static void multiplikation()
+            //{
+            //    Console.WriteLine("Indtast tal 1");
+            //    string tal1 = Console.ReadLine();
 
-                Console.WriteLine("Indtast tal 2");
-                string tal2 = Console.ReadLine();
+            //    Console.WriteLine("Indtast tal 2");
+            //    string tal2 = Console.ReadLine();
 
-                double t1 = System.Convert.ToDouble(tal1);
-                double t2 = System.Convert.ToDouble(tal2);
+            //    double t1 = System.Convert.ToDouble(tal1);
+            //    double t2 = System.Convert.ToDouble(tal2);
 
-                double sum = t1 * t2;
+            //    double sum = t1 * t2;
 
-                Console.WriteLine("Resultatet er: " + sum);
-            }
+            //    Console.WriteLine("Resultatet er: " + sum);
+            //}
 
-            static void division()
-            {
-                Console.WriteLine("Indtast tal 1");
-                string tal1 = Console.ReadLine();
+            //static void division()
+            //{
+            //    Console.WriteLine("Indtast tal 1");
+            //    string tal1 = Console.ReadLine();
 
-                Console.WriteLine("Indtast tal 2");
-                string tal2 = Console.ReadLine();
+            //    Console.WriteLine("Indtast tal 2");
+            //    string tal2 = Console.ReadLine();
 
-                double t1 = System.Convert.ToDouble(tal1);
-                double t2 = System.Convert.ToDouble(tal2);
+            //    double t1 = System.Convert.ToDouble(tal1);
+            //    double t2 = System.Convert.ToDouble(tal2);
 
-                double sum = t1 / t2;
+            //    double sum = t1 / t2;
 
-                Console.WriteLine("Resultatet er: " + sum);
-            }
+            //    Console.WriteLine("Resultatet er: " + sum);
+            //}
 
 
         }
