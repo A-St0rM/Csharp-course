575d2bdb2a6780fc119219fbd944225935028e1a
AlissaStorm <Alissaalidib@hotmail.com>
2024-09-16 21:12:55 +0200

    Compiler working again

--- csharpCourse/Program.cs
+++ csharpCourse/Program.cs
@@ -7,47 +7,74 @@ namespace csharpCourse
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Vælg en regneoperation: ");
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraktion");
-            Console.WriteLine("3. Multiplikation");
-            Console.WriteLine("4. Division");
-            double input = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Indtast tal 1");
-            double t1 = Convert.ToDouble(Console.ReadLine()); 
-
-            Console.WriteLine("Indtast tal 2");
-            double t2 = Convert.ToDouble(Console.ReadLine());
- 
-            //userInput();
-
-            if (input == 1)
-            {              
-                double result = addition(t1, t2);
-                Console.WriteLine("Resultatet er: " + result);
-
-            }
-            else if (input == 2)
+            bool state = true;
+            while (state)
             {
-                //subtraktion();
-            }
-            else if (input == 3)
-            {
-               // multiplikation();
-            }
-            else if (input == 4)
-            {
-                //division();
+
+                Console.WriteLine("Vælg en regneoperation: ");
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Subtraktion");
+                Console.WriteLine("3. Multiplikation");
+                Console.WriteLine("4. Division");
+                Console.WriteLine("5. Exit");
+                double input = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Indtast tal 1");
+                double t1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Indtast tal 2");
+                double t2 = Convert.ToDouble(Console.ReadLine());
+
+                
+
+                if (input == 1)
+                {
+                    double result = addition(t1, t2);
+                    Console.WriteLine("Resultatet er: " + result);
+                    Console.ReadKey();
+                    Console.Clear();
+
+                }
+                else if (input == 2)
+                {
+                    //subtraktion();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input == 3)
+                {
+                    // multiplikation();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input == 4)
+                {
+                    //division();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input == 5)
+                {
+                    state = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldigt input");
+                }
+
             }
 
-            
+
 
             double addition(double t1, double t2) //Skal ændre retur type og tilføje parametre
-            {             
+            {
                 return t1 + t2;
-                
+
+            }
+
+            double substraktion(double t1, double t2)
+            {
+                return t1 - t2;
             }
 
             //static void subtraktion()
